5a89bb61767d97a6f08b9d110383029b10993277
Onkar Bharatesh <onkarbk1@gmail.com>
2022-03-07 08:56:30 +0530

    new tic-tac-toe

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,69 +1,63 @@
-import numpy 
-board=numpy.array([['-', '-', '-'], ['-', '-', '-'], ['-', '-', '-']])
+a=[]
+for i in range(3):
+  a.append(['-','-','-'])
+def prints():
+  for i in range(3):
+    for j in range(3):
+      print(a[i][j],end=" ")
+    print()
+prints()
 player1='X'
 player2='O'
-chance = 0
-j=0
+count=0
 k=0
-x=0
-y=0
-while(chance<9 or x!=0 or y!=0):
-    a,b=map(int,input("Player 1  where do u wanna put your 'X'? ").split())
-    chance=chance+1
-    board[a][b]='X'
-    print(board)
-    if(board[0][0]==board[1][1] and board[1][1]==board[2][2] and board[1][1]=='X'):
-        print("Player1 wins")
-        x=1
-        break
-    elif(board[0][2]==board[1][1] and board[1][1]==board[2][0] and board[0][2]=='X'):
-        print("Player1 wins")
-        x=1
-        break
-    for i in range(3):
-        if(board[i][0]==board[i][1] and board[i][0]==board[i][2] and board[i][0]=='X'):
-            print("Player1 wins")
-            x=1
-            break
-    if(x==1):
-        break
-    for j in range(3):
-        if(board[0][j]==board[1][j] and board[1][j]==board[2][j] and board[0][j]=='X'):
-            print("Player1 wins")
-            x=1
-            break
-    if(x==1):
-        break
-            
-    c,d=map(int,input("Player 2  where do u wanna put your 'O'? ").split())
-    board[c][d]='O'
-    chance=chance+1
-    print(board)
-    i=0
-    j=0
-    k=0
-    if(board[0][0]==board[1][1] and board[1][1]==board[2][2] and board[1][1]=='O'):
-        print("Player2 wins")
-        y=1
-        break
-    elif(board[0][2]==board[1][1] and board[1][1]==board[2][0] and board[0][2]=='O'):
-        print("Player2 wins")
-        y=1
-        break
-    for i in range(3):
-        if(board[i][0]==board[i][1] and board[i][0]==board[i][2] and board[i][0]=='O'):
-            print("Player2 wins")
-            y=1
-            break
-    if(y==1):
-        break
-    for j in range(3):
-        if(board[0][j]==board[1][j] and board[1][j]==board[2][j] and board[0][j]=='O'):
-            print("Player2 wins")
-            y=1
-            break
-    if(y==1):
-        break
-if(chance>=9):
-    print("Match withdraw")
-    
+r=0
+while count<=9:
+  x,y=map(int,input("Player1 Enter the position where do u wanna put 'X'?").split())
+  a[x][y]=player1
+  prints()
+  count+=1
+  if(a[0][0]==a[1][1] and a[2][2]==a[1][1] and a[0][0]==player1):
+    print("Player1 wins")
+    k=1
+    break
+  for i in range(3):
+    if(a[i][0]==a[i][1] and a[i][2]==a[i][1] and a[i][0]==player1):
+      print("Player1 wins")
+      k=1
+      break
+  for i in range(3):
+    if(a[0][i]==a[1][i] and a[2][i]==a[1][i] and a[0][i]==player1):
+      print("Player1 wins")
+      k=1
+      break
+  if(a[0][2]==a[1][1] and a[2][0]==a[1][1] and a[2][0]==player1):
+    print("Player1 wins")
+    k=1
+    break
+  if(k==1):
+    break
+  m,n=map(int,input("Player2 Enter the position where do u wanna put 'O'?").split())
+  a[m][n]=player2
+  prints()
+  count+=1
+  if(a[0][0]==a[1][1] and a[2][2]==a[1][1] and a[0][0]==player2):
+    print("Player2 wins")
+    r=1
+    break
+  for i in range(3):
+    if(a[i][0]==a[i][1] and a[i][2]==a[i][1] and a[i][0]==player2):
+      print("Player2 wins")
+      r=1
+      break
+  for i in range(3):
+    if(a[0][i]==a[1][i] and a[2][i]==a[1][i] and a[0][i]==player2):
+      print("Player2 wins")
+      r=1
+      break
+  if(a[0][2]==a[1][1] and a[2][0]==a[1][1] and a[2][0]==player2):
+    print("Player2 wins")
+    r=1
+    break
+  if(count==9):
+    print("Draw match")
